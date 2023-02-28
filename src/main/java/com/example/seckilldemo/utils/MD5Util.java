cae755da36ce381562dd7523c6b0d82e6a2eaec3
LiGangSTU <lg18335414176@sina.com>
2023-02-28 16:50:20 +0800

    登录验证，抛出异常

--- src/main/java/com/example/seckilldemo/utils/MD5Util.java
+++ src/main/java/com/example/seckilldemo/utils/MD5Util.java
@@ -1,41 +1,47 @@
 package com.example.seckilldemo.utils;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.stereotype.Component;
 
+/**
+ * MD5工具类
+ */
+@Component
 public class MD5Util {
-    public static String md5(String str){return DigestUtils.md5Hex(str);}
+
+    public static String md5(String str) {
+        return DigestUtils.md5Hex(str);
+    }
+
     private static final String salt = "1a2b3c4d";
 
     /**
      * 第一次加密
-     * @param inputPass
-     * @return
-     */
-    public static String inputPassToFromPass(String inputPass){
-        String str = salt.charAt(0)+ salt.charAt(2) + inputPass + salt.charAt(5) + salt.charAt(4);
+     **/
+    public static String inputPassToFromPass(String inputPass) {
+        String str = salt.charAt(0) + salt.charAt(2) + inputPass + salt.charAt(5) + salt.charAt(4);
         return md5(str);
     }
 
     /**
      * 第二次加密
-     * @param formPass
-     * @param salt
-     * @return
-     */
+     **/
     public static String formPassToDBPass(String formPass, String salt) {
         String str = salt.charAt(0) + salt.charAt(2) + formPass + salt.charAt(5) + salt.charAt(4);
         return md5(str);
     }
 
-    public static String inputPassToDBPass(String inputPass, String salt){
-        String frompass = inputPassToFromPass(inputPass);
-        String dbpass = formPassToDBPass(frompass,salt);
-        return dbpass;
+    public static String inputPassToDBPass(String inputPass, String salt) {
+        String fromPass = inputPassToFromPass(inputPass);
+        String dbPass = formPassToDBPass(fromPass, salt);
+        return dbPass;
     }
 
     public static void main(String[] args) {
         System.out.println(inputPassToFromPass("123456"));//d3b1294a61a07da9b49b6e22b2cbd7f9
-        System.out.println(formPassToDBPass(inputPassToFromPass("123456"), "1a2b3c4d"));
+        System.out.println(formPassToDBPass("ce21b747de5af71ab5c2e20ff0a60eea", "1a2b3c4d"));
         System.out.println(inputPassToDBPass("123456", "1a2b3c4d"));
     }
+
+
 }
