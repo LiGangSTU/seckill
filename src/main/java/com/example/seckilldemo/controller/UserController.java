abebaeba664ccd735c35763d8bda79e490ad3d8b
LiGangSTU <lg18335414176@sina.com>
2023-03-06 10:06:22 +0800

    测试RabbitMQ不同模式

--- src/main/java/com/example/seckilldemo/controller/UserController.java
+++ src/main/java/com/example/seckilldemo/controller/UserController.java
@@ -2,10 +2,14 @@ package com.example.seckilldemo.controller;
 
 
 import com.example.seckilldemo.pojo.User;
+import com.example.seckilldemo.rabbitmq.MQReceiver;
+import com.example.seckilldemo.rabbitmq.MQSender;
 import com.example.seckilldemo.vo.RespBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -18,9 +22,21 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private MQSender mqSender;
+
     @RequestMapping("/info")
     public RespBean info(User user){
         return RespBean.success(user);
     }
 
+    /**
+     * 测试RabbitMQ
+     */
+    @RequestMapping("/mq/fanout")
+    @ResponseBody
+    public void mq() {
+        mqSender.send("Hello");
+    }
 }
