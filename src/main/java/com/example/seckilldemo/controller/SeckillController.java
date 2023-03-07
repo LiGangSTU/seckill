60be42aafb60f0b7e63eb52824a3a6f2d82f2298
LiGangSTU <lg18335414176@sina.com>
2023-03-07 09:31:26 +0800

    增加redis-Lua脚本，修改redis逻辑 Lua脚本 Lua脚本优势： 使用方便，Redis内置了对Lua脚本的支持 Lua脚本可以在Rdis服务端原子的执行多个Redis命令 由于网络在很大程度上会影响到Redis性能，使用Lua脚本可以让多个命令一次执行，可以有 效解决网络给Redis带来的性能问题 使用Lua脚本思路： 提前在Redis服务端写好Lua脚本，然后在java客户端去调用脚本 可以在java客户端写Lua脚本，写好之后，去执行。需要执行时，每次将脚本发送到Redis上 去执行

--- src/main/java/com/example/seckilldemo/controller/SeckillController.java
+++ src/main/java/com/example/seckilldemo/controller/SeckillController.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -47,6 +49,8 @@ public class SeckillController implements InitializingBean {
     private RedisTemplate redisTemplate;
     @Autowired
     private MQSender mqSender;
+    @Autowired
+    private RedisScript<Long> redisScript;
 
     private Map<Long, Boolean> EmptyStockMap = new HashMap<>();
 
@@ -70,10 +74,11 @@ public class SeckillController implements InitializingBean {
                return RespBean.error(RespBeanEnum.EMPTY_STOCK);
            }
            //预减内存
-           Long stock = valueOperations.decrement("seckillGoods:"+ goodsId);
+           Long stock = (Long) redisTemplate.execute(redisScript,
+                   Collections.singletonList("seckillGoods:"+goodsId), Collections.EMPTY_LIST);
            if(stock < 0){
                EmptyStockMap.put(goodsId, true);
-               valueOperations.increment("seckillGoods:"+goodsId);
+               //valueOperations.increment("seckillGoods:"+goodsId);
                return RespBean.error(RespBeanEnum.EMPTY_STOCK);
            }
            //请求入队，立即返回排队中
